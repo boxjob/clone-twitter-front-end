f636d1c2b19bf6ec409fe324df55b2ebe546b7e4
Ricardo <ricardo@iMac-de-Ricardo.local>
2025-07-08 17:38:32 -0300

    add aside profile

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -1,10 +1,34 @@
+import { NavItem } from "@/components/nav/nav-Item";
+import { NavLogout } from "@/components/nav/nav-logout";
+import { NavProfile } from "@/components/nav/nav-profile";
+import { Logo } from "@/components/ui/logo";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import { ReactNode } from "react";
 
 export default function Layout({ children }:{ children:ReactNode}) {
   return (
     <main className="min-h-screen flex justify-center mx-auto max-w-7xl">
         <aside className="hidden lg:flex flex-col stick top-0 h-screen w-72 px-3 border-r-2 border-gray-900">
-            ESQUERDA
+            <div className="flex-1 mt-6">
+                <Logo size={24} />
+                <nav className="mt-11">
+                    <NavItem
+                        href="/home"
+                        icon={faHouse}
+                        label="Página inicial"
+                    />
+                    <NavItem
+                        href="/profile"
+                        icon={faUser}
+                        label="Meu perfil"
+                    />
+                </nav>
+            </div>
+            <div className="mb-6 flex flex-col gap-4">
+                <NavLogout/>
+                <NavProfile />
+            </div>
         </aside>
         <section className="flex-1 max-w-lg">
             { children }
---
+++ src/data/user.ts
@@ -0,0 +1,11 @@
+import { User } from "@/types/user";
+
+export const user:User = {
+    slug:'boxjob',
+    name:'Ricardo Alexandre',
+    avatar: './avatar.png',
+    cover:'./banner-perfil.png',
+    bio:'Desenvolvedor fullstack há mais de 5 anos.',
+    link: 'http://boxjob.com.br',
+    postCount: 123
+}
